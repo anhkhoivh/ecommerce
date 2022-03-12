4f18755ad9398cb0074081f9c4328c024c43a255
anhkhoivh <vuhoanganhkhoi11@gmail.com>
2022-03-12 22:04:40 +0700

    first commit

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
+import { AccountService } from './account/account.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'My View - Store';
+  constructor(private basketService: BasketService, private accountService: AccountService) { }
+
+  ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    // const fn = this.accountService.loadCurrentUser(token!)
+    // fn.subscribe({
+    //   next: () => {
+    //     console.log("a")
+    //   }, error: (err: any) => console.log(err), complete: () => { }
+    // });
+  }
+
+  loadBasket() {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(() => {
+      }, error => console.log(error));
+    }
+  }
+}
